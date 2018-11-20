373eca40724dd8b4831a07bc67bc855855dc697d
aaronwharton <aircitybruno@gmail.com>
2018-11-20 17:48:53 +0800

    add exercise in go-pro - add template exercise on src/example/web test5.go test6.go

---
+++ go-pro/src/example/web/test5.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+	"runtime"
+)
+
+type Person struct {
+	PersonName string
+	email      string
+}
+
+func main() {
+	// 调用导出字段会输出该字段的数据，
+	// 当调用未导出字段（email）或者不存在的字段（Email）
+	// 输出就为空而不报错（go version=go1.11.2）
+	// 有的说未导出字段应该是报错，我猜是版本不同的原因
+	t := template.New("new template")
+	t, _ = t.Parse("Hello {{.PersonName}}!{{.Email}}!!{{.email}}!!!")
+	s := Person{"Aaron", "zhd@qq.com"}
+	t.Execute(os.Stdout, s) // Output:Hello Aaron!
+	// 检查当前使用的golang版本
+	fmt.Println(runtime.Version())
+}
---
+++ go-pro/src/example/web/test6.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+)
+
+type Friend struct {
+	FriendName string
+}
+
+type People struct {
+	PeopleName string
+	Emails     []string
+	Friends    []*Friend
+}
+
+func main() {
+	// 条件处理if-else
+	// ⚠️：与下面的警号标志结合起来看
+	tEmpty, _ := template.New("template test").Parse("空 pipeline if demo: {{if ``}} 不会输出. {{end}}\n")
+	//tEmpty = template.Must(tEmpty.Parse("空 pipeline if demo: {{if ``}} 不会输出. {{end}}\n"))
+	tEmpty.Execute(os.Stdout, nil)
+	// 空 pipeline if demo:
+
+	tWithValue := template.New("template test")
+	tWithValue = template.Must(tWithValue.Parse("不为空的 pipeline if demo: {{if `anything`}} 我有内容，我会输出. {{end}}\n"))
+	tWithValue.Execute(os.Stdout, nil)
+	// 不为空的 pipeline if demo:  我有内容，我会输出.
+
+	tIfElse := template.New("template test")
+	tIfElse = template.Must(tIfElse.Parse("if-else demo: {{if `anything`}} if部分 {{else}} else部分.{{end}}\n"))
+	tIfElse.Execute(os.Stdout, nil)
+	// if-else demo:  if部分
+
+	// Must操作：
+	// 检测模版是否正确（大括号是否匹配等语法错误）
+	// ⚠️：可与正则的Compile、MustCompile结合起来看：不用Must返回一个template和一个error，使用Must则忽略产生的error（直接panic），两者只是应用场景不同。
+	// regexp.Compile()返回正则结构体和一个error，
+	// 而regexp.MustCompile()只返回一个正则结构体。
+	fmt.Println("new 1 parsed ok.")
+	template.Must(template.New("new 1").Parse("new 1 parse and /* comment */"))
+
+	fmt.Println("new 2 parsed ok.")
+	template.Must(template.New("new 2").Parse("new 2 parse and variable {{.FriendName}}"))
+
+	// 会产生panic，因为Must解析时发现了语法错误
+	fmt.Println("new 3 caused panic because of variable {{.FriendName} not be placed correctly.")
+	//template.Must(template.New("new 3").Parse("new 3 parse and variable {{.FriendName}"))
+
+	// 输出嵌套字段内容，使用类似golang的for-range语法
+	f1 := Friend{"Aaron"}
+	f2 := Friend{"Allen"}
+	t := template.New("new template file created")
+	t, _ = t.Parse(`Hello {{.PeopleName}}!
+	{{range .Emails}}
+		an email {{.}}
+	{{end}}
+	{{with .Friends}}
+	{{range .}}
+		my friend name is {{.FriendName}}
+	{{end}}
+	{{end}}`)
+	p := People{"people", []string{"zhd@qq.com", "zhd@163.com"}, []*Friend{&f1, &f2}}
+	t.Execute(os.Stdout, p)
+}
+
+// Output:
+//Hello people!
+//
+//an email zhd@qq.com
+//
+//an email zhd@163.com
+//
+//
+//
+//my friend name is Aaron
+//
+//my friend name is Allen
+//
+//
